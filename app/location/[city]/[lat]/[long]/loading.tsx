20653a71a0cc45d6be08408f9c82fea29ae75f85
phoenixjw <phoenixjw9320@gmail.com>
2023-05-22 11:30:02 +0100

    Corrected spelling mistsakes

--- app/location/[city]/[lat]/[long]/loading.tsx
+++ app/location/[city]/[lat]/[long]/loading.tsx
@@ -11,7 +11,7 @@ function Loading() {
             Loading City Weather Information
         </h1>
         <h2 className='text-xl font-bolf text-center mb-10 animate-pulse'>
-            Hold on, we are crunchinhg the numbers & generating an AI summary of the weather!
+            Hold on, we are crunching the numbers & generating an AI summary of the weather!
         </h2>
 
     </div>
